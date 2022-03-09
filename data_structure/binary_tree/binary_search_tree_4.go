0b2d54dfface81291327326cc5a8375b45ed25aa
zhanghan <pikazhanghan@didiglobal.com>
2022-03-09 16:01:28 +0800

    二叉搜索树树-扁平化嵌套列表迭代器

--- data_structure/binary_tree/binary_search_tree_4.go
+++ data_structure/binary_tree/binary_search_tree_4.go
@@ -118,3 +118,92 @@ func recSerialize(root *TreeNode, buffer *bytes.Buffer) {
  * data := ser.serialize(root);
  * ans := deser.deserialize(data);
  */
+
+/**
+题目：扁平化嵌套列表迭代器
+
+给你一个嵌套的整数列表 nestedList 。每个元素要么是一个整数，要么是一个列表；该列表的元素也可能是整数或者是其他列表。请你实现一个迭代器将其扁平化，使之能够遍历这个列表中的所有整数。
+实现扁平迭代器类 NestedIterator ：
+NestedIterator(List<NestedInteger> nestedList) 用嵌套列表 nestedList 初始化迭代器。
+int next() 返回嵌套列表的下一个整数。
+boolean hasNext() 如果仍然存在待迭代的整数，返回 true ；否则，返回 false 。
+你的代码将会用下述伪代码检测：
+
+	initialize iterator with nestedList
+	res = []
+	while iterator.hasNext()
+	    append iterator.next() to the end of res
+	return res
+如果 res 与预期的扁平化列表匹配，那么你的代码将会被判为正确。
+
+*/
+
+// NestedInteger
+// 这个接口允许创建嵌套列表。
+// 你不应该实现它，或者猜测它的实现
+type NestedInteger struct {
+	integer int
+	list    []*NestedInteger
+}
+
+// IsInteger 如果这个NestedInteger包含一个整数，而不是一个嵌套的列表，则返回true。
+func (this NestedInteger) IsInteger() bool {
+	return this.integer != 0
+}
+
+// GetInteger
+// 返回这个NestedInteger保存的单个整数，如果它保存单个整数
+// 如果这个NestedInteger持有一个嵌套的列表，则结果为undefined
+// 所以在调用这个方法之前，你应该有一个检查
+func (this NestedInteger) GetInteger() int {
+	return this.integer
+}
+
+// SetInteger 设置这个NestedInteger为一个整数。
+func (n *NestedInteger) SetInteger(value int) {
+	n.integer = value
+}
+
+// Add 设置这个NestedInteger保存一个嵌套的列表，并向它添加一个嵌套的整数。
+func (this *NestedInteger) Add(elem NestedInteger) {
+	this.list = append(this.list, &elem)
+}
+
+// GetList
+// 返回这个NestedInteger持有的嵌套列表，如果它持有一个嵌套列表
+// 如果这个NestedInteger包含一个整数，则列表长度为0
+// 如果你想修改NestedInteger的List元素，你可以直接访问它
+func (this NestedInteger) GetList() []*NestedInteger {
+	return this.list
+}
+
+type NestedIterator struct {
+	Vals []int
+}
+
+func Constructor1(nestedList []*NestedInteger) *NestedIterator {
+	var nestedIterator NestedIterator
+	var traverseIterator func(nestedList []*NestedInteger)
+	traverseIterator = func(nestedList []*NestedInteger) {
+		for _, nestedInteger := range nestedList {
+			if nestedInteger.IsInteger() {
+				nestedIterator.Vals =
+					append(nestedIterator.Vals, nestedInteger.GetInteger())
+			} else {
+				traverseIterator(nestedInteger.GetList())
+			}
+		}
+	}
+	traverseIterator(nestedList)
+	return &nestedIterator
+}
+
+func (this *NestedIterator) Next() int {
+	val := this.Vals[0]
+	this.Vals = this.Vals[1:]
+	return val
+}
+
+func (this *NestedIterator) HasNext() bool {
+	return len(this.Vals) > 0
+}
