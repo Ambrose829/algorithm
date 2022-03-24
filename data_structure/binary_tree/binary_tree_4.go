4ade237809906707ba06df1c3b5d3fd9d9569b98
zhanghan <pikazhanghan@didiglobal.com>
2022-03-24 11:56:31 +0800

    二叉搜索树树-最近公共祖先

---
+++ data_structure/binary_tree/binary_tree_4.go
@@ -0,0 +1,39 @@
+package binary_tree
+
+/**
+  @Author: pika
+  @Date: 2022/3/23 1:01 下午
+  @Description:
+*/
+
+/*
+	给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+	百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
+*/
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root == p || root == q {
+		return root
+	}
+
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+
+	if left == nil && right == nil {
+		return nil
+	}
+
+	if left != nil && right != nil {
+		return root
+	}
+
+	if left != nil {
+		return left
+	} else {
+		return right
+	}
+}
